266aae6abf76aeb785979169c7e8b2ccebc33431
Valdecir Correa <valdecir.correa@voleep.com>
2023-10-14 18:22:09 -0300

    Remove unused localizer initialization

--- lib/carclean_app.dart
+++ lib/carclean_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:voleep_carclean_frontend/core/states/providers/localization_provider.dart';
 import 'package:voleep_carclean_frontend/routing/routes/go_router_provider.dart';
 import 'package:voleep_carclean_frontend/shared/theme/themes.dart';
 
@@ -10,7 +9,6 @@ class CarCleanApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final goRouter = ref.read(goRouterProvider);
-    final localization = ref.read(localizationProvider);
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
@@ -19,8 +17,6 @@ class CarCleanApp extends ConsumerWidget {
       theme: lightTheme,
       darkTheme: darkTheme,
       routerConfig: goRouter,
-      localizationsDelegates: localization.localizationsDelegates,
-      supportedLocales: localization.supportedLocales,
     );
   }
 }
